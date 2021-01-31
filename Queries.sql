e7f4e080610e7bec0ee8368de4e2e4483779e329
Batool AlObaid <batool-998@hotmail.com>
2021-01-31 08:22:49 +0300

    docs: Add second query to queries file

--- Queries.sql
+++ Queries.sql
@@ -12,3 +12,18 @@ ON r.inventory_id=i.inventory_id
 WHERE name IN ('Animation','Children','Classics','Comedy','Family','Music')
 GROUP BY 1
 ORDER BY 1;
+
+/*Query 2- query used for second insight*/
+SELECT standard_quartile, COUNT(film_id) AS movies_count
+FROM (
+    SELECT f.film_id, NTILE(4) OVER w1 AS standard_quartile
+    FROM category c
+    JOIN film_category fc
+    ON c.category_id = fc.category_id
+    JOIN film f
+    ON f.film_id = fc.film_id
+    WHERE c.name IN ('Animation','Children','Classics','Comedy','Family','Music')
+    WINDOW w1 AS (ORDER BY f.rental_duration)
+) sub
+GROUP BY 1
+ORDER BY 1;
